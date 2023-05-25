1daaede41d4de37d1a8c07aa346aa372981b8b99
Jack Zhang <zhiyi.zhang.duke@gmail.com>
2023-05-24 23:24:13 -0400

    Fixed issues with gpt-3-encoder and started using gpt-tokenizer instead

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { merge } = require('webpack-merge');
+const path = require('path'); // import path module if it is not already done
 
 const common = require('./webpack.common.js');
 const PATHS = require('./paths');
@@ -12,9 +13,16 @@ const config = (env, argv) =>
       popup: PATHS.src + '/popup.js',
       contentScript: PATHS.src + '/contentScript.js',
       background: PATHS.src + '/background.js',
-    },
+      'gpt-tokenizer': './node_modules/gpt-tokenizer/dist/cl100k_base.js', // Update the path to gpt-tokenizer
+    },    
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: '[name].js',
+      sourceMapFilename: '[file].map' // Changed from [name].js.map to [file].map to avoid filename conflict
+    },    
     devtool: argv.mode === 'production' ? false : 'source-map',
     resolve: {
+      modules: ['./src', './node_modules'],
       fallback: {
         fs: false,
         path: require.resolve('path-browserify'),
--- src/popup.js
+++ src/popup.js
@@ -1,112 +1,22 @@
 'use strict';
 
 import './popup.css';
+import { encode } from 'gpt-tokenizer'
 
 (function () {
-  // We will make use of Storage API to get and store `count` value
-  // More information on Storage API can we found at
-  // https://developer.chrome.com/extensions/storage
-
-  // To get storage access, we have to mention it in `permissions` property of manifest.json file
-  // More information on Permissions can we found at
-  // https://developer.chrome.com/extensions/declare_permissions
-  const counterStorage = {
-    get: (cb) => {
-      chrome.storage.sync.get(['count'], (result) => {
-        cb(result.count);
+  document.addEventListener("DOMContentLoaded", function() {
+    var submitButton = document.getElementById("submitButton");
+    submitButton.addEventListener("click", function() {
+        var promptInput = document.getElementById("promptInput").value;
+        var rawTextInput = document.getElementById("tokenizedInput").value;
+        
+        const encodedInput = encode(rawTextInput); // encode input
+    
+        // Log the prompt and tokenized text array to the console
+        console.log("Prompt:", promptInput);
+        console.log("Tokenized Input:", rawTextInput)
+        console.log("Number of characters:", rawTextInput.length)
+        console.log("Tokens:", encodedInput); // log tokens
       });
-    },
-    set: (value, cb) => {
-      chrome.storage.sync.set(
-        {
-          count: value,
-        },
-        () => {
-          cb();
-        }
-      );
-    },
-  };
-
-  function setupCounter(initialValue = 0) {
-    document.getElementById('counter').innerHTML = initialValue;
-
-    document.getElementById('incrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'INCREMENT',
-      });
-    });
-
-    document.getElementById('decrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'DECREMENT',
-      });
-    });
-  }
-
-  function updateCounter({ type }) {
-    counterStorage.get((count) => {
-      let newCount;
-
-      if (type === 'INCREMENT') {
-        newCount = count + 1;
-      } else if (type === 'DECREMENT') {
-        newCount = count - 1;
-      } else {
-        newCount = count;
-      }
-
-      counterStorage.set(newCount, () => {
-        document.getElementById('counter').innerHTML = newCount;
-
-        // Communicate with content script of
-        // active tab by sending a message
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
-
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              type: 'COUNT',
-              payload: {
-                count: newCount,
-              },
-            },
-            (response) => {
-              console.log('Current count value passed to contentScript file');
-            }
-          );
-        });
-      });
-    });
-  }
-
-  function restoreCounter() {
-    // Restore count value
-    counterStorage.get((count) => {
-      if (typeof count === 'undefined') {
-        // Set counter value as 0
-        counterStorage.set(0, () => {
-          setupCounter(0);
-        });
-      } else {
-        setupCounter(count);
-      }
-    });
-  }
-
-  document.addEventListener('DOMContentLoaded', restoreCounter);
-
-  // Communicate with background file by sending a message
-  chrome.runtime.sendMessage(
-    {
-      type: 'GREETINGS',
-      payload: {
-        message: 'Hello, my name is Pop. I am from Popup.',
-      },
-    },
-    (response) => {
-      console.log(response.message);
-    }
-  );
+  });
 })();
